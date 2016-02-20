df75f1f4cea715bf6a95a97975197590331f80eb
Chaitanya <you@example.com>
2016-02-20 11:06:26 -0800

    first commit with initial set

---
+++ loader.s
@@ -0,0 +1,34 @@
+global loader                   ; the entry symbol for ELF
+
+MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
+FLAGS        equ 0x0            ; multiboot flags
+CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
+                                ; (magic number + checksum + flags should equal 0)
+KERNEL_STACK_SIZE equ 4096			; size of kernel you expect to take
+
+section .bss
+align 4                                     ; align at 4 bytes
+kernel_stack:                               ; label points to beginning of memory
+		resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
+
+section .text:                  ; start of the text (code) section
+align 4                         ; the code must be 4 byte aligned
+    dd MAGIC_NUMBER             ; write the magic number to the machine code,
+    dd FLAGS                    ; the flags,
+    dd CHECKSUM                 ; and the checksum
+
+dword3 equ	3
+dword2 equ	2
+dword1 equ	1
+
+
+loader:                         ; the loader label (defined as entry point in linker script)
+		mov esp, kernel_stack + KERNEL_STACK_SIZE
+		extern sum_of_three
+
+		push dword3
+		push dword2
+		push dword1
+		call sum_of_three
+.loop:
+    jmp .loop                   ; loop forever
